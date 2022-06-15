938d23c120b571ce2b9663ea8fd59e0c71d6f388
WiseThoughts <102035978+WiseThoughts@users.noreply.github.com>
2022-06-16 00:32:30 +0100

    IT'S WORKING!!!!
    Got the page to load with the footer, navbar, about, contact and home. Also added the api stuff and got that loading and working (I need to learn how to spell SRC!!)

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,51 +1,66 @@
 import React, { useEffect, useState } from "react";
-import { Image } from "../css/Api.styled.js";
+import { Image, Container } from "../css/Api.styled.js";
 import "../css/App.css";
 import { faker } from "@faker-js/faker";
 
-function Api() {
-  const catName = faker.name.firstName();
-  const catType = faker.animal.cat();
-  const color = faker.color.rgb();
-  const word = faker.word.adjective();
 
-  const [cat, setCat] = useState([]);
-  const [error, setError] = useState(null);
+function Api (){
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://api.thecatapi.com/v1/images/search?limit=10"
-        );
-        if (!response.ok) {
-          throw new Error(response.statusText);
+
+const [cats, setCats] = useState([]);
+const [error, setError] = useState(null);
+const [basketItems, setBasketItems] = useState([]);
+
+const addToBasket = (item) => {
+    setBasketItems([...basketItems, item]);
+};
+
+const fetchImages = async ()=>{
+    try {
+        const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=20");
+        if (!response.ok){
+        throw new Error(response.statusText)
         }
         const data = await response.json();
-        setCat(data);
-        console.log(data);
-      } catch (error) {
+        return data;
+    } catch (error) {
         console.log(error);
-        setError("Couldn't fetch the data");
-      }
+        setError("Couldn't fetch data(error 404)");}
+    };
+    useEffect(() => {
+        const fetchData = async () => {
+        let catData = await fetchImages();
+        catData = catData.map((cats) => {
+            cats.name = faker.name.firstName();
+            cats.gender = faker.name.gender(true);
+            cats.catType = faker.animal.cat();
+            cats.word = faker.word.adjective();
+            cats.price = faker.commerce.price();
+            return cats;
+        });
+        setCats(catData);
     };
     fetchData();
-  }, []);
+}, []);
 
-  return (
+
+
+    return (
     <div>
-      {cat.map((cats) => (
-        <div className="container" key={cats.id}>
-          <h4 color={color}>{catName}</h4>
-          <Image src={cats.url} alt="Image of a cat" />
-          <h5>{catType}</h5>
-          <p>{word}</p>
+        {cats.map((cat,) => {
+        return <div>
+          <h3>{cat.name}</h3>
+          <Image src={cat.url} alt="cat pic"/>
+          <h4>${cat.price}</h4>
+          <h5>{cat.catType}</h5>
+          <h5>{cat.gender}</h5>
+          <h5>{cat.word}</h5>
+          <button>Take Me Home</button>
         </div>
-      ))}
-
-      {error && <p>{error}</p>}
+        })}
+        {error && <p>{error}</p>}
     </div>
-  );
-}
+    )}
+
 
 export default Api;

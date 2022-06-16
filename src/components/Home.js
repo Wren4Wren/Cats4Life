d01933c2b76a6153d99ee7d76b4ef84bac891de2
Safwan Arif <safwan.arif.786@gmail.com>
2022-06-16 15:56:58 +0100

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
 
 
 const Home = () => {
-    return ( 
+    return (
         <div>
-        
-        </div>
-    );
+            <h1>Welcome to The Kitty Palace</h1>
+            <br></br>
+            <br></br>
+            <img src="./images/catLogo.png" alt = "logo" ></img>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p>Purchase your friendly feline here and receive a month's supply of cat food.</p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3>Enter Shop</h3>
+        </div>);
 }
 
 export default Home;

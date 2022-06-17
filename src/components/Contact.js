835d4cfe6ee3ab6644ba039733d630f432884ee9
Wren4Wren <104135667+Wren4Wren@users.noreply.github.com>
2022-06-17 18:33:51 +0100

    Create Contact.js
    cleaned up to resolve conflicts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-
-
 const Contact = () => {
     const [sender_name, set_sender_name] = useState('');
     const [sende_email, set_sender_email] = useState('');
@@ -27,17 +25,14 @@ const Contact = () => {
     
     return ( 
         <div>
-            <header> Contact Us</header>
-            <span>
+            <h1 className="header"> Contact Us</h1>
             <form onSubmit={sendMail}>
                 <input type="text" name="sender_name" value={sender_name} onChange={handleName} required placeholder="Your name" />
                 <input type="email" name ="send_email" value={sende_email} onChange={handleEmail} required placeholder="your email id" />
-                <p>
                 <textarea name="message" value={message} onChange={handleMessage} required placeholder="your message" />
-                <button type="sumbit">Send mail</button>
-                </p>
+                <button className:"ContactBtn" type="sumbit">Send mail</button>
+
             </form>
-            </span>
         </div>
     );
 }
